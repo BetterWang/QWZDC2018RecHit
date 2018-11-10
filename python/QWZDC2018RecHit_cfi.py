80fd2028f7dca27e89c4fc90a2a918e7f5c175ab
Quan Wang <BetterWang@gmail.com>
2018-11-10 22:43:14 +0100

    [+] update

--- python/QWZDC2018RecHit_cfi.py
+++ python/QWZDC2018RecHit_cfi.py
@@ -5,6 +5,6 @@ from ZDC2018Gain_cfg import gainFill7427
 QWzdcreco = cms.EDProducer('QWZDC2018RecHit',
         srcDetId = cms.untracked.InputTag("zdcdigi", "DetId"),
         srcfC = cms.untracked.InputTag("zdcdigi", "regularfC"),
-        ZDCCalib = gainFill7427
+        ZDCCalib = gainFill7435
         )
 
